0e10c348d56fe3c31541285bce7d1dbb48175e67
alipk26 <aliefkw26@gmail.com>
2024-06-11 21:00:44 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getSurahList, findSimilarSurahByName } from "./api";
 
-function App() {
+const App = () => {
+  const [surahList, setSurahList] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
+  const [foundSurah, setFoundSurah] = useState([]);
+  const [expandedSurah, setExpandedSurah] = useState(null);
+  const [surahNotFound, setSurahNotFound] = useState(false);
+
+  useEffect(() => {
+    getSurahList().then((result) => {
+      setSurahList(result);
+    }).catch((error) => {
+      console.error('Error fetching surah list:', error);
+      setSurahList([]);
+    });
+  }, []);
+
+  const handleInputChange = (e) => {
+    setSearchInput(e.target.value);
+  }
+
+  const handleSearch = () => {
+    const surah = findSimilarSurahByName(surahList, searchInput);
+    if (surah.length === 0) {
+      setSurahNotFound(true);
+      setFoundSurah(surahList);
+    } else {
+      setSurahNotFound(false);
+      setFoundSurah(surah);
+    }
+    setExpandedSurah(null); // Reset state expandedSurah saat melakukan pencarian baru
+  }
+
+  const toggleSurahDetails = (index) => {
+    if (expandedSurah === index) {
+      setExpandedSurah(null); // Tutup detail jika sudah terbuka
+    } else {
+      setExpandedSurah(index);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Daftar Surah</h1>
+        <input type="text" value={searchInput} onChange={handleInputChange} placeholder="Masukkan nama surah" />
+        <button onClick={handleSearch}>Cari</button>
+        {surahNotFound && <p>Surah tidak ditemukan.</p>}
+        {foundSurah.length > 0 && (
+          <div className="Surah-list">
+            {foundSurah.map((surahItem, index) => (
+              <div className='Surah-wrapper' key={index}>
+                <div className='Surah-nama' onClick={() => toggleSurahDetails(index)}>
+                  {surahItem.nama}
+                </div>
+                {expandedSurah === index && (
+                  <div className='Surah-details'>
+                    <div>Arti: {surahItem.arti}</div>
+                    <div>Asma: {surahItem.asma}</div>
+                    <div>Tipe: {surahItem.type}</div>
+                    <div>Keterangan: <span dangerouslySetInnerHTML={{ __html: surahItem.keterangan }} /></div>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
       </header>
     </div>
   );
---
+++ src/api.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+
+const baseUrl = process.env.REACT_APP_BASEURL;
+
+export const getSurahList = async () => {
+  try {
+    const response = await axios.get(baseUrl);
+    console.log({response})
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching the surah list", error);
+    return [];
+  }
+};
+
+
+// Fungsi untuk mencari surah berdasarkan input yang mirip
+export const findSimilarSurahByName = (surahList, name) => {
+  if (!surahList || surahList.length === 0) {
+    return [];
+  }
+
+  const similarSurah = surahList.reduce((acc, surahItem) => {
+    // Menghapus spasi dari nama surah dan input
+    const surahNameWithoutSpace = surahItem.nama.toLowerCase().replace(/\s/g, '');
+    const searchNameWithoutSpace = name.toLowerCase().replace(/\s/g, '');
+    
+    // Memeriksa Levenshtein distance antara nama surah dan input tanpa spasi
+    const distance = levenshteinDistance(surahNameWithoutSpace, searchNameWithoutSpace);
+    if (distance <= 3) { // Toleransi jarak 3 karakter
+      acc.push(surahItem);
+    }
+    return acc;
+  }, []);
+
+  return similarSurah.length > 0 ? similarSurah : surahList;
+};
+
+
+// Fungsi untuk menghitung Levenshtein distance antara dua string
+const levenshteinDistance = (a, b) => {
+  const distanceMatrix = Array(b.length + 1).fill(null).map(() => Array(a.length + 1).fill(null));
+
+  for (let i = 0; i <= a.length; i += 1) {
+    distanceMatrix[0][i] = i;
+  }
+
+  for (let j = 0; j <= b.length; j += 1) {
+    distanceMatrix[j][0] = j;
+  }
+
+  for (let j = 1; j <= b.length; j += 1) {
+    for (let i = 1; i <= a.length; i += 1) {
+      const indicator = a[i - 1] === b[j - 1] ? 0 : 1;
+      distanceMatrix[j][i] = Math.min(
+        distanceMatrix[j][i - 1] + 1, // Deletion
+        distanceMatrix[j - 1][i] + 1, // Insertion
+        distanceMatrix[j - 1][i - 1] + indicator // Substitution
+      );
+    }
+  }
+
+  return distanceMatrix[b.length][a.length];
+};
+
